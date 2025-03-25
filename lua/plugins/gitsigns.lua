efa8369fcfa38bb1e7fe3f353f9665db4bb005ac
anton-pokhodun <anton.pokhodun@nytimes.com>
2025-03-25 20:07:26 +0200

    Configure gitsigns, navigator, treesitter, tmux-navigator

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,82 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require("gitsigns").setup({
+      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+      numhl = true,      -- Toggle with `:Gitsigns toggle_numhl`
+      linehl = true,     -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      watch_gitdir = {
+        follow_files = true,
+      },
+      auto_attach = true,
+      attach_to_untracked = true,
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+      sign_priority = 6,
+      update_debounce = 100,
+      status_formatter = nil,  -- Use default
+      max_file_length = 40000, -- Disable if file is longer than this (in lines)
+      preview_config = {
+        -- Options passed to nvim_open_win
+        border = "single",
+        style = "minimal",
+        relative = "cursor",
+        row = 0,
+        col = 1,
+      },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]c", function()
+          if vim.wo.diff then
+            return "]c"
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
+
+        map("n", "[c", function()
+          if vim.wo.diff then
+            return "[c"
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
+
+        -- Actions
+        map("n", "<leader>hr", gs.reset_hunk)
+        map("n", "<leader>hS", gs.stage_buffer)
+        map("n", "<S-h>", gs.next_hunk)
+        map("n", "<leader>hp", gs.preview_hunk)
+        map("n", "<leader>tb", gs.toggle_current_line_blame)
+        map("n", "<leader>hd", gs.diffthis)
+        map("n", "<leader>hD", function()
+          gs.diffthis("~")
+        end)
+        map("n", "<leader>td", gs.toggle_deleted)
+
+        -- Text object
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+      end,
+    })
+  end,
+}
--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -28,15 +28,101 @@ return {
   },
 
   -- disable trouble
-  { "folke/trouble.nvim", enabled = false },
+  { "folke/trouble.nvim",      enabled = false },
 
   -- override nvim-cmp and add cmp-emoji
   {
     "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",  -- cmp_nvim_lsp
+      "neovim/nvim-lspconfig", -- lspconfig
+      "hrsh7th/cmp-emoji",
+      "onsails/lspkind-nvim",  -- lspkind (VS pictograms)
+      {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+        dependencies = { "rafamadriz/friendly-snippets" }, -- Snippets
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+          -- https://github.com/rafamadriz/friendly-snippets/blob/main/snippets/go.json
+        end,
+      },
+      { "saadparwaiz1/cmp_luasnip", enabled = true },
+    },
+    config = function()
+      local luasnip = require("luasnip")
+      local types = require("luasnip.util.types")
+
+      -- Display virtual text to indicate snippet has more nodes
+      luasnip.config.setup({
+        ext_opts = {
+          [types.choiceNode] = {
+            active = { virt_text = { { "⇥", "GruvboxRed" } } },
+          },
+          [types.insertNode] = {
+            active = { virt_text = { { "⇥", "GruvboxBlue" } } },
+          },
+        },
+      })
+
+      local cmp = require("cmp")
+      local lspkind = require("lspkind")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+        }),
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 70,
+            show_labelDetails = true,
+          }),
+        },
+      })
+
+      local lspconfig = require("lspconfig")
+
+      -- All languages: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+
+      -- Default lspconfig values for Go are set by `navigator`
+      -- Go: go install golang.org/x/tools/gopls@latest
+
+      -- Python: brew install pyright
+      lspconfig["pyright"].setup({})
+
+      -- Ruby: gem install solargraph
+      lspconfig["solargraph"].setup({})
+
+      -- https://phpactor.readthedocs.io/en/master/usage/standalone.html#installation
+      lspconfig["phpactor"].setup({})
     end,
   },
 
@@ -84,7 +170,7 @@ return {
       init = function()
         require("lazyvim.util").lsp.on_attach(function(_, buffer)
           -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
         end)
       end,
@@ -194,5 +280,22 @@ return {
       },
     },
   },
-  { "christoomey/vim-tmux-navigator" },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 }
